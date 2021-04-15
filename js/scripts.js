830ef524da345adf7057bbc3557a3c2ad56876e8
Stewart ilondanga <stilondanga@gmail.com>
2021-04-15 21:38:23 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -24,23 +24,23 @@ cells.forEach((cell, index) => {
 function setNewMessageTimer() {
   // Generate a random time when the new message appears
   timeTill = Math.floor(Math.random() * 10000);
-  /*
-    setTimeout(function(event) {
-      // select random cell
-      var cellIndex = Math.floor(Math.random() * 9);
-      // get the alert element of the cell
-      var cellToModifiy = cells[cellIndex].children[0];
-      // Add 1 to the value of the element
-      cellToModifiy.innerHTML = parseInt(cellToModifiy.innerHTML) + 1;
-      // Make sure cell isn't hidden
-      cellToModifiy.classList.remove('hidden');
-      // Add the animation class
-      cellToModifiy.classList.add('update');
-      // Once the animation is complete, remove the class for the animation
-      setTimeout(function(e) {
-        cellToModifiy.classList.remove('update');
-      }, 300 );
-      // Set a new timer for the next message
-      setNewMessageTimer();
-    }, timeTill);
-  }
+
+  setTimeout(function(event) {
+    // select random cell
+    var cellIndex = Math.floor(Math.random() * 9);
+    // get the alert element of the cell
+    var cellToModifiy = cells[cellIndex].children[0];
+    // Add 1 to the value of the element
+    cellToModifiy.innerHTML = parseInt(cellToModifiy.innerHTML) + 1;
+    // Make sure cell isn't hidden
+    cellToModifiy.classList.remove('hidden');
+    // Add the animation class
+    cellToModifiy.classList.add('update');
+    // Once the animation is complete, remove the class for the animation
+    setTimeout(function(e) {
+      cellToModifiy.classList.remove('update');
+    }, 300);
+    // Set a new timer for the next message
+    setNewMessageTimer();
+  }, timeTill);
+}
